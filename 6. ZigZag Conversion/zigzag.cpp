decb28fa0e93818d1e31e940cdd41c298f4a2bb0
ezboy4life <82066433+ezboy4life@users.noreply.github.com>
2023-11-09 11:49:11 -0300

    Update zigzag.cpp

--- 6. ZigZag Conversion/zigzag.cpp
+++ 6. ZigZag Conversion/zigzag.cpp
@@ -4,11 +4,19 @@ using namespace std;
 class Solution {
 public:
     string convert(string s, int numRows) {
-        char zigzag[5][5];
+        // cout << numRows / 2;
+        // cout << numRows + (numRows / 2);
+        for (int i = 0; i < s.size(); i = i + numRows + (numRows / 2)){
+            cout << s[i];
+        }
+
+        return "a";
     }
 };
 
 int main() {
+    Solution solution;
+    solution.convert("PAYPALISHIRING", 3);
     
     system("pause");
     return 0;
