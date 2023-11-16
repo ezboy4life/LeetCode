f9adad803fdf3c65759f861c15c61d916643304b
ezboy4life <82066433+ezboy4life@users.noreply.github.com>
2023-11-16 10:13:38 -0300

    Add 1980

---
+++ 1980. Find Unique Binary String/find_unique.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    string findDifferentBinaryString(vector<string>& nums) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    solution.findDifferentBinaryString({"01", "10"});
+
+    return 0;
+}
